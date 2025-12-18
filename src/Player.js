bf5d0398e6a7069adaa6004078fdbd7cc5ee59c6
robinsond1995d-sudo <robinsond1995d@gmail.com>
2025-12-18 16:08:13 +0330

    Add files via upload

---
+++ src/Player.js
@@ -0,0 +1,30 @@
+
+import * as THREE from 'https://cdn.skypack.dev/three';
+
+export class Player {
+  constructor(camera) {
+    this.object = new THREE.Object3D();
+    this.object.add(camera);
+
+    this.camera = camera;
+    this.health = 100;
+    this.rage = 0;
+
+    this.velocity = new THREE.Vector3();
+    this.speed = 4.5;
+
+    this.dashCooldown = 0;
+  }
+
+  dash(dir) {
+    if (this.dashCooldown > 0) return;
+    this.velocity.add(dir.multiplyScalar(6));
+    this.dashCooldown = 1;
+  }
+
+  update(delta) {
+    this.dashCooldown -= delta;
+    this.object.position.add(this.velocity.clone().multiplyScalar(delta));
+    this.velocity.multiplyScalar(0.88);
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+import { Game } from './Game.js';
+
+const container = document.getElementById('game');
+const game = new Game(container);
+
+function loop(time) {
+  game.update(time * 0.001);
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
