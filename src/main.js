4790871227141ae8bd823bf4871236405d663b7e
robinsond1995d-sudo <robinsond1995d@gmail.com>
2025-12-19 12:10:16 +0330

    ASHMA – Doom-style final build (Dash, Glory Kill, Boss)

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { Game } from './Game.js';
+
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resize();
+window.addEventListener('resize', resize);
+
+const game = new Game(canvas, ctx);
+game.start();
